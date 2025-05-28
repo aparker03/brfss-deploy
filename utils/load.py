4c509c60fe29870fff4c1194a88e554fb6ca5eb6
Alexis Parker <aparker0917@gmail.com>
2025-05-28 00:52:22 -0700

    Add sample dataset toggle for faster deployment

--- utils/load.py
+++ utils/load.py
@@ -1,9 +1,17 @@
 import pandas as pd
 import streamlit as st
 
+# Flag to toggle between full and sample dataset
+USE_SAMPLE = True  # Set to False for full dataset
+
+# Dropbox links
+FULL_DATA_URL = "https://www.dropbox.com/scl/fi/7uu8km9qqinzpn3lcfl88/brfss_2022.csv?rlkey=er6mrhhizmhk6i9sn2coobo1h&st=65awlrqm&dl=1"
+SAMPLE_DATA_URL = "https://www.dropbox.com/scl/fi/7qyci8spfk60szrj0nsml/brfss_sample.csv?rlkey=6dvfakgoqepu893vtrb134j28&st=zuqqjzrm&dl=1"  # Replace with your own sample file link
+
 @st.cache_data
 def load_data():
-    df = pd.read_csv("https://www.dropbox.com/scl/fi/7uu8km9qqinzpn3lcfl88/brfss_2022.csv?rlkey=er6mrhhizmhk6i9sn2coobo1h&st=u4gu6drn&dl=1")
+    url = SAMPLE_DATA_URL if USE_SAMPLE else FULL_DATA_URL
+    df = pd.read_csv(url)
     df.columns = df.columns.str.upper().str.strip()
 
     df = df[df["_SEX"].isin([1.0, 2.0])]
